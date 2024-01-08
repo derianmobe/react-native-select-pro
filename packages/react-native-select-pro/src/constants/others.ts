5c3d3cf145d9f1532fb31e73ae27d73e1fbfad1f
Derian Molina <dmolina@MacBook-Pro.local>
2024-01-08 11:51:11 -0600

    Status bar heigth

--- packages/react-native-select-pro/src/constants/others.ts
+++ packages/react-native-select-pro/src/constants/others.ts
@@ -1,10 +1,5 @@
-import { NativeModules, Platform } from 'react-native';
-
 // based on: https://github.com/callstack/react-native-paper/blob/main/src/constants.tsx
-const statusBarHeight =
-    NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants?.statusBarHeight ?? 0;
+// const statusBarHeight =
+//     NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants?.statusBarHeight ?? 0;
 
-export const APPROX_STATUSBAR_HEIGHT: number = Platform.select({
-    android: statusBarHeight,
-    ios: Platform.Version < 11 ? statusBarHeight : 0,
-});
+export const APPROX_STATUSBAR_HEIGHT: number = 0;
